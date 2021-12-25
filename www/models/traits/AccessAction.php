2060cec1cca64300343ba795b499ed379ef3a669
tarasovc47 <tarasovc47@live.ru>
2021-12-25 20:22:25 +0500

    Доработаны правила доступа к экшенам по принципу "гость/не гость"

--- www/models/traits/AccessAction.php
+++ www/models/traits/AccessAction.php
@@ -8,9 +8,24 @@ trait AccessAction
 {
     public function checkAction($view)
     {
-        if (App::$components->session->isGuest() && !empty($this->authAction))
+        if (App::$components->session->isGuest() && !empty($this->authAction)) { // если пользователь - гость и список экшенов для авторизованных не пуст
+            foreach ($this->authAction as $action) { // то обрабатываем этот список
+                $this->check($view, $action); // и сверяем
+            }
+        }
+        elseif (!empty($this->guestAction)) //если список экшенов для гостей не пуст
         {
-            echo 'true';
+            foreach ($this->guestAction as $action) { //то обрабатываем этот список
+                $this->check($view, $action); // и сверяем
+            }
+        }
+    }
+    private function check($view, $action)
+    {
+        if ($view == $action) // если нашёлся экшен, который есть в списке гостевых экшенов ИЛИ в списке авторизованных экшенов и юзер при этом гость
+        {
+            $referer = App::$components->request->referer;
+            return header("Location: {$referer}"); // то отправляем откуда пришёл
         }
     }
 }
